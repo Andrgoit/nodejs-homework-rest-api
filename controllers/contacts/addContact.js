451f950735172d665e1180234dcb50fcbead2ae4
Andrgoit <stakgrom@gmail.com>
2022-12-08 23:44:08 +0200

    come clearing

--- controllers/contacts/addContact.js
+++ controllers/contacts/addContact.js
@@ -1,23 +1,9 @@
 const contacts = require("../../models/contacts");
 
-// const { HttpError } = require("../../helpers/");
-
-// импортируем схему  валидации
-// const { addContactSchema } = require("../../schemas/contacts");
-
 const addContact = async (req, res, next) => {
-  // try {
   const newContact = req.body;
-  // const { error } = addContactSchema.validate(newContact);
-
-  // if (error) {
-  //   throw HttpError(400, error.message);
-  // }
   const result = await contacts.addContact(newContact);
   return res.status(201).json(result);
-  // } catch (error) {
-  //   next(error);
-  // }
 };
 
 module.exports = addContact;
--- middlewares/validateParams.js
+++ middlewares/validateParams.js
@@ -1,7 +1,6 @@
 const { HttpError } = require("../helpers");
 
 const validateParams = (req, res, next) => {
-  console.log(req);
   const { contactId } = req.params;
   if (!contactId) {
     next(HttpError(400, "Bad Request"));
