cc4fab74761ff1d9495e8cc346178524963e237d
Andrgoit <stakgrom@gmail.com>
2023-01-09 22:51:06 +0200

    fix

--- controllers/auth/updateAvatar.js
+++ controllers/auth/updateAvatar.js
@@ -7,9 +7,13 @@ const User = require("../../models/user");
 const avatarsDir = path.join(__dirname, "../../", "public", "avatars");
 
 const resizeAvatar = async (avatar) => {
-  Jimp.read(avatar).then((image) => {
-    return image.resize(250, 250).write(avatar);
-  });
+  Jimp.read(avatar)
+    .then((image) => {
+      return image.resize(250, 250).write(avatar);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 };
 
 const updateAvatar = async (req, res) => {
@@ -17,17 +21,18 @@ const updateAvatar = async (req, res) => {
   const { path: tempUpload, originalname } = req.file;
 
   const avatarName = `${_id}_${originalname}`;
-  const resultUpload = path.join(avatarsDir, avatarName);
 
-  await fs.rename(tempUpload, resultUpload);
-
-  await resizeAvatar(resultUpload);
-
-  const avatarURL = path.join("avatars", avatarName);
-
-  await User.findByIdAndUpdate(_id, { avatarURL });
-
-  res.json({ avatarURL });
+  try {
+    const resultUpload = path.join(avatarsDir, avatarName);
+    await fs.rename(tempUpload, resultUpload);
+    await resizeAvatar(resultUpload);
+    const avatarURL = path.join("avatars", avatarName);
+    await User.findByIdAndUpdate(_id, { avatarURL });
+    res.json({ avatarURL });
+  } catch (error) {
+    await fs.unlink(tempUpload);
+    throw error;
+  }
 };
 
 module.exports = updateAvatar;
